aeb8d9870191a023260f7a5e2ef74bafed714c19
ByeongwooJang <a01066278824@gmail.com>
2017-04-23 04:27:51 +0900

    Update IconTextView.java

--- Structure Health Care_Android/app/src/main/java/com/example/gwnu/finalproject/IconTextView.java
+++ Structure Health Care_Android/app/src/main/java/com/example/gwnu/finalproject/IconTextView.java
@@ -18,13 +18,9 @@ import android.widget.TextView;
  */
 
 
-//IconTextView 자바 클래스는 무엇을 어떤 값에서 보여줄지를 설정해주는 역할
-
 
 public class IconTextView extends LinearLayout{
 
-
-    //private란 변경이 되면 안 되는 변수에 대해 접근 제한 자 설정
 
     private TextView mText01;
 
@@ -40,16 +36,12 @@ public class IconTextView extends LinearLayout{
         super(context);
 
 
-        //보일 해당 listView에 들어갈 디자인 레이아웃 xml 설정(만들어진 listitem.xml을 보일 디자인 레이아웃으로 설정하였음)
-
         LayoutInflater inflater = (LayoutInflater)
 
                 context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         inflater.inflate(R.layout.listitem, this, true);
 
-
-        //각 변수를 설정하고 그 변수가 어떤 값을 의미하는지 또, 무엇을 읽어오고 보여주는지를 설정
 
 
         mText01 = (TextView) findViewById(R.id.dataItem01);
@@ -73,10 +65,7 @@ public class IconTextView extends LinearLayout{
     }
 
 
-    //위에 방법과 구성은 동일한 방법이지만 조건 문과 파라메타 값으로 자동적으로 셋팅을 해주는것
-
-    //파라메타로 Adapter 자바에서 설정된 int형 index 값과 String형 데이터 값을 가져와서 위처럼 변수를 자동으로 설정해줌
-
+    //setting automatically about parameters
     public void setText(int index, String data){
 
         if (index == 0 ){
